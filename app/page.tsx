7e7a0b69a7b2bd85affcb351a061641e6c69014d
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2026-01-20 22:03:15 +0100

    Refactor unused code after fixing proxy to verify cookie

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,10 @@
-import { createClient } from "@/lib/supabase-server";
 import Login from "./components/auth/Login";
-import { logout } from "./actions/actions";
-
 export default async function Home() {
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  console.log(user);
-
   return (
     <main className="px-3 md:w-[80%] md:mx-auto">
       <h1>Welcome to my Schedule app!</h1>
       <section>
-        {user ? (
-          <>
-            <button onClick={logout}>Log out</button>
-          </>
-        ) : (
-          <Login />
-        )}
+        <Login />
       </section>
     </main>
   );
