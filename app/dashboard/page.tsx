848eb0fffe063a6d8c5436440170a2d0d7e9d0c0
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2026-01-23 21:54:45 +0100

    add styling for logout button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,18 @@
-import { logout } from '../actions/actions';
-import { getAuthClaims } from '@/lib/dal/user-dal';
+import { logout } from "../actions/actions";
+import { getAuthClaims } from "@/lib/dal/user-dal";
 
 async function page() {
   const user = await getAuthClaims();
   return (
-    <main>
+    <main className="p-5">
       <h1>Welcome {user.email}</h1>
       <p>You have the role of: [ {user?.user_role} ]</p>
-      <button onClick={logout}>Log out</button>
+      <button
+        onClick={logout}
+        className="bg-red-600 text-white p-2 px-3 rounded cursor-pointer"
+      >
+        Log out
+      </button>
     </main>
   );
 }
