d3feb3d5bfe9cf7966d92fee8eff675483735086
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2026-02-07 22:01:07 +0100

    update user dal and dto

--- lib/dal/user-dal.ts
+++ lib/dal/user-dal.ts
@@ -1,8 +1,8 @@
 // Data access layer to fetch data to the server/client?
 
-import { createClient } from '@/lib/supabase-server';
-import { AuthClaimsDTO } from './user-dto';
-import { cache } from 'react';
+import { createClient } from "@/lib/supabase-server";
+import { AuthClaimsDTO, UserDTO } from "./user-dto";
+import { cache } from "react";
 
 export const getAuthClaims = cache(async (): Promise<AuthClaimsDTO> => {
   const supabase = await createClient();
@@ -10,8 +10,28 @@ export const getAuthClaims = cache(async (): Promise<AuthClaimsDTO> => {
   const { data, error } = await supabase.auth.getClaims();
 
   if (error || !data?.claims) {
-    throw new Error('An error accured, no valid token');
+    throw new Error("An error accured, no valid token");
   }
 
   return data?.claims as AuthClaimsDTO;
+});
+
+export const getAuthenticatedUser = cache(async (): Promise<UserDTO> => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    throw new Error("Unauthorized");
+  }
+
+  return {
+    id: user.id,
+    email: user.email ?? "",
+    createdAt: user.created_at,
+    role: user.app_metadata.user_role,
+  };
 });
