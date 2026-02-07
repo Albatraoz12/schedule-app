9fbe97f1300973cc50e17245986146230c083ec8
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2026-02-07 22:01:23 +0100

    update funktion for better streaming

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,18 +1,20 @@
-import { getAuthClaims } from "@/lib/dal/user-dal";
+import { getAuthClaims, getAuthenticatedUser } from "@/lib/dal/user-dal";
 import { getLessions, getRooms } from "@/lib/dal/lessions/lessions-dal";
 import { logout } from "@/app/actions/actions";
 import CreateLession from "../components/CreateLession";
 import LessionCalendar from "./components/LessionCalendar";
 
 async function page() {
-  const user = await getAuthClaims();
-  const lessions = await getLessions();
-  const rooms = await getRooms();
+  const [user, lessions, rooms] = await Promise.all([
+    getAuthenticatedUser(),
+    getLessions(),
+    getRooms(),
+  ]);
 
   return (
     <main className="p-5">
       <h1>Welcome {user?.email}</h1>
-      <p>You have the role of: [ {user?.user_role} ]</p>
+      <p>You have the role of: [ {user?.role} ]</p>
       <button
         onClick={logout}
         className="bg-red-600 text-white p-2 px-3 rounded cursor-pointer"
