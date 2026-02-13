3764b0179fb136eb36ea8c5f558e4c56704b2522
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2026-02-13 08:26:23 +0100

    add styling to login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Login from "./components/auth/Login";
 export default async function Home() {
   return (
-    <main className="px-3 md:w-[80%] md:mx-auto">
-      <h1>Welcome to my Schedule app!</h1>
-      <section>
+    <main className="px-3 md:w-[80%] md:mx-auto flex flex-col justify-start mt-5 md:mt-0 md:justify-center items-center w-full h-screen my-auto">
+      <h1 className="text-xl pb-10 md:text-3xl">Welcome to my Schedule app!</h1>
+      <section className="w-full">
         <Login />
       </section>
     </main>
