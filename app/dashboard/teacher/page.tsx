625e0a8e6b20e86d5d8932b0270a16251a5a50d6
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2026-02-16 21:46:31 +0100

    add content to teacher page

--- app/dashboard/teacher/page.tsx
+++ app/dashboard/teacher/page.tsx
@@ -1,26 +1,55 @@
-import { getAuthClaims } from "@/lib/dal/user/user-dal";
 import { getLessions, getRooms } from "@/lib/dal/lessions/lessions-dal";
-import { logout } from "@/app/actions/actions";
+import { getAuthenticatedUser } from "@/lib/dal/user/user-dal";
+import { Suspense } from "react";
 import CreateLession from "../components/CreateLession";
+import LessionCalendar from "../components/LessionCalendar";
+import { redirect } from "next/navigation";
+import Logout from "@/app/components/auth/Logout";
+import getClasses from "@/lib/dal/class/class-dal";
 
-async function page() {
-  const user = await getAuthClaims();
-  const lessions = await getLessions();
-  const rooms = await getRooms();
+export default async function AdminPage() {
+  const user = await getAuthenticatedUser();
+
+  if (!user) redirect("/");
 
   return (
     <main className="p-5">
       <h1>Welcome {user?.email}</h1>
-      <p>You have the role of: [ {user?.user_role} ]</p>
-      <button
-        onClick={logout}
-        className="bg-red-600 text-white p-2 px-3 rounded cursor-pointer"
-      >
-        Log out
-      </button>
-      <CreateLession rooms={rooms} />
+      <p>You have the role of: [ {user?.role} ]</p>
+      <Logout />
+
+      <Suspense fallback={<div>Loading lessons...</div>}>
+        <LessionsData userId={user.id} userRole={user.role} />
+      </Suspense>
     </main>
   );
 }
 
-export default page;
+export async function LessionsData({
+  userId,
+  userRole,
+}: {
+  userId: string;
+  userRole: string;
+}) {
+  const [lessions, rooms, classes] = await Promise.all([
+    getLessions(),
+    getRooms(),
+    getClasses(),
+  ]);
+
+  return (
+    <>
+      {userRole && (userRole === "admin" || userRole === "teacher") && (
+        <CreateLession rooms={rooms} classes={classes} />
+      )}
+      <section className="my-4">
+        <LessionCalendar
+          lessions={lessions || []}
+          rooms={rooms || []}
+          userId={userId}
+        />
+      </section>
+    </>
+  );
+}
